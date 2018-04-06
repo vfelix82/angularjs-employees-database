bcd947590cbb7143e4c891c6393c75ea2216ac4e
Victor Felix <vfelix82@gmail.com>
2018-04-06 09:34:23 -0700

    add working validation for department dropdown box

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+
+import { Department } from '../models/department.model'
 import { Employee } from '../models/employee.model';
 
 
@@ -24,6 +26,14 @@ export class CreateEmployeeComponent implements OnInit {
       isActive: null,
       photoPath: null
     };
+
+    departments: Department[] = [
+      { id: 1, name: 'Help Desk' },
+      { id: 2, name: 'HR' },
+      { id: 3, name: 'IT' },
+      { id: 4, name: 'Payroll' },
+      { id: 5, name: 'Admin' }
+    ];
 
 
   constructor() {
